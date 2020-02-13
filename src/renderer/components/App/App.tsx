5218c1cf15973841a911439db619487945c139f6
Pierre Chabiland <pierre@casusludi.com>
2020-02-13 12:47:44 +0100

    better not saving alert

--- src/renderer/components/App/App.tsx
+++ src/renderer/components/App/App.tsx
@@ -144,7 +144,7 @@ class App extends Component<AppProps, AppState> {
 					<div className="layout__header-title">
 						Cardmaker Studio
 						{this.props.project && <div className="project-bar__name">
-							- {this.props.project.name}{this.props.project.modified && <span className="project-bar__modified">(not saved)</span>}
+							- {this.props.project.name}{this.props.project.modified && <span className="project-bar__modified">(not saved)<i className="icon fas fa-exclamation-triangle"></i></span>}
 						</div>}
 						
 					</div>

cc111e6c8009f7f2517a0725d7bf107027cae97c
Pierre Chabiland <pierre@casusludi.com>
2020-02-19 12:36:13 +0100

    Improve UI in editor

--- src/renderer/components/EditorPanel/TemplateEditor/TemplateEditor.tsx
+++ src/renderer/components/EditorPanel/TemplateEditor/TemplateEditor.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ProjectTemplate, ProjectFiles, ProjectLayout } from '../../../services/Project';
 import TabNav, { TabNavItem, TabNavHeaderPosition } from '../../Misc/TabNav/TabNav';
 import CodeEditor from '../CodeEditor/CodeEditor';
+import './TemplateEditor.scss';
 
 
 export type TemplateEditorProps = {
@@ -19,10 +20,13 @@ export default function TemplateEditor(props: TemplateEditorProps) {
     return (
         <React.Fragment>
             {props.template ?
-                <TabNav className="TemplateEditor__Tabs full-space" headerPosition={TabNavHeaderPosition.TOP} >
-                    <TabNavItem label="HTML"><CodeEditor width={props.width} className="full-space" mode="handlebars" onChange={(code) => props.template && props.onFileChanged(props.template.hbs, code)} code={props.files[props.template.hbs].content} /></TabNavItem>
-                    <TabNavItem label="Styles"><CodeEditor width={props.width} className="full-space" mode="css" onChange={(code) => props.template && props.onFileChanged(props.template.styles, code)} code={props.files[props.template.styles].content} /></TabNavItem>
-                </TabNav> :
+                <div className="TemplateEditor full-space">
+                    <div className="TemplateEditor__header"><span className="TemplateEditor__header-label">{props.template.id}</span></div>
+                    <TabNav className="TemplateEditor__Tabs" headerPosition={TabNavHeaderPosition.TOP} >
+                        <TabNavItem label="HTML"><CodeEditor width={props.width} className="full-space" mode="handlebars" onChange={(code) => props.template && props.onFileChanged(props.template.hbs, code)} code={props.files[props.template.hbs].content} /></TabNavItem>
+                        <TabNavItem label="Styles"><CodeEditor width={props.width} className="full-space" mode="css" onChange={(code) => props.template && props.onFileChanged(props.template.styles, code)} code={props.files[props.template.styles].content} /></TabNavItem>
+                    </TabNav>
+                </div> :
                 <div>No Template</div>
             }
         </React.Fragment>

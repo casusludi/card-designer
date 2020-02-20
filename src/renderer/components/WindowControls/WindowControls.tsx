96a4cd4b594cb18f474f65472c50d948391ee636
Pierre Chabiland <pierre@casusludi.com>
2020-02-20 15:02:18 +0100

    remove unused import

--- src/renderer/components/WindowControls/WindowControls.tsx
+++ src/renderer/components/WindowControls/WindowControls.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import './WindowControls.scss';
 import { remote } from "electron";
 
